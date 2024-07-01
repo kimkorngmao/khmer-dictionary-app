4b299c47cd2dd6d32e892102c12f43f4d6f0f6be
Kimkorng Mao <giimguangmao@gmail.com>
2024-07-01 19:27:55 +0700

    add a warning message

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,13 @@ import { SearchBar } from "./components/SearchBar";
 export default function Home() {
 
     return (
-        <div className="w-full h-screen flex items-center justify-center">
-            <div className="w-full max-w-screen-md p-4">
+        <div className="w-full h-screen flex sm:items-center justify-center">
+            <div className="w-full max-w-screen-md py-10 px-4 sm:p-4">
                 <h1 className="moul text-gray-800 text-center text-3xl mb-10 moul-regular">វចនានុក្រមខ្មែរ</h1>
                 <SearchBar />
+                <div className="p-4 my-5 bg-gray-100 border border-gray-200 rounded-md">
+                    <p className="text-gray-600 text-sm leading-6">សូមអភ័យទោស! ពាក្យមួយចំនួនអាចនឹងមានបញ្ហាមិនប្រក្រតីកើតមានឡើង ឬមិនមាននៅក្នុងកម្មវិធីដោយពុំទាន់បញ្ចូលក្នុង Wiktionary API នៅឡើយ។</p>
+                </div>
                 <Footer />
             </div>
         </div>
